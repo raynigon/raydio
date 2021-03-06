1bef086d2825c5e8eb40d5985bdeb9f14601cb7f
Simon Schneider <10846939+raynigon@users.noreply.github.com>
2021-03-06 14:22:10 +0100

    Optimize Buffer
    Shrink Buffer on write instead of read

--- src/main/kotlin/io/rayd/backend/webradio/io/ByteQueue.kt
+++ src/main/kotlin/io/rayd/backend/webradio/io/ByteQueue.kt
@@ -3,6 +3,12 @@ package io.rayd.backend.webradio.io
 import java.nio.BufferOverflowException
 import kotlin.math.min
 
+/**
+ * ByteQueue implementations queues bytes
+ *
+ * This implementation assumes that the read operation is more time critical than the write operation.
+ * Therefore the internal buffer is shrunk on write instead of read.
+ * */
 class ByteQueue(size: Int = 0) {
 
     private var writePtr = 0
@@ -11,6 +17,12 @@ class ByteQueue(size: Int = 0) {
 
     @Synchronized
     fun write(data: ByteArray, offset: Int, length: Int) {
+        // Move existing Buffer, so the readPtr is zero
+        val remaining = writePtr - readPtr
+        System.arraycopy(buffer, readPtr, buffer, 0, remaining)
+        writePtr = remaining
+        readPtr = 0
+        // Copy the new content
         val newWritePtr = writePtr + length
         if (newWritePtr > buffer.size) throw BufferOverflowException()
         System.arraycopy(data, offset, buffer, writePtr, length)
@@ -22,10 +34,6 @@ class ByteQueue(size: Int = 0) {
         val size = min(writePtr - readPtr, length)
         System.arraycopy(buffer, readPtr, data, offset, size)
         readPtr += size
-        val remaining = writePtr - readPtr
-        System.arraycopy(buffer, readPtr, buffer, 0, remaining)
-        writePtr = remaining
-        readPtr = 0
         return size
     }
 
