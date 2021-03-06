14e7e97b7e9cc540005881a0371e2a7cf01b7cab
Simon Schneider <10846939+raynigon@users.noreply.github.com>
2021-03-06 13:11:37 +0100

    Fix #4 Use Jetty instead of Tomcat

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,27 +25,48 @@ repositories {
 }
 
 dependencies {
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-websocket")
-    implementation("org.springframework.boot:spring-boot-starter-webflux")
-    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+    // Koltin
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+
+    // Spring Boot starter
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-web") {
+        exclude("org.springframework.boot:spring-boot-starter-tomcat")
+    }
+    implementation("org.springframework.boot:spring-boot-starter-jetty")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+
+    // Jackson
+    implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+
+    // Database
     implementation("org.liquibase:liquibase-core")
     implementation("org.xerial:sqlite-jdbc:3.34.0")
     implementation("com.github.gwenn:sqlite-dialect:0.1.1")
+
+    // Mapstruct
     implementation("org.mapstruct:mapstruct:1.4.1.Final")
-    implementation("commons-io:commons-io:2.8.0")
-    implementation("org.apache.httpcomponents:httpclient:4.5.13")
 
     // Sound Libraries
     implementation("com.googlecode.soundlibs:mp3spi:1.9.5.4")
-    testImplementation("org.testcontainers:spock:1.15.1")
+
+    // Utils
+    implementation("commons-io:commons-io:2.8.0")
+    implementation("org.apache.httpcomponents:httpclient:4.5.13")
+
+    // Spring Boot Test starter
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // Spock
     testImplementation("org.codehaus.groovy:groovy:3.0.7")
     testImplementation("org.spockframework:spock-core:2.0-M4-groovy-3.0")
     testImplementation("org.spockframework:spock-spring:2.0-M4-groovy-3.0")
-    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    // Testcontainer
+    testImplementation("org.testcontainers:spock:1.15.1")
+
+    // Wiremock
     testImplementation("com.github.tomakehurst:wiremock:2.27.2")
     testImplementation("com.github.tomjankes:wiremock-groovy:0.2.0")
 
